1cec4d199c2c122a764e8ba32fc5ce16e160e4f7
ydavpacat <ydavpacat21@gmail.com>
2021-10-06 19:25:41 +0200

    CPA pract 1

--- CPA/pract1/material/lanzador.sh
+++ CPA/pract1/material/lanzador.sh
@@ -15,7 +15,22 @@ OMP_NUM_THREADS=4 ./pintegral 1 50000000
 echo ""
 OMP_NUM_THREADS=8 ./pintegral 1 50000000
 echo ""
+OMP_NUM_THREADS=16 ./pintegral 1 50000000
+echo ""
 echo ""
 echo "###########################"
-echo "# Metodo de integración 1 #"
+echo "# Metodo de integración 2 #"
 echo "###########################"
+echo ""
+echo ""
+OMP_NUM_THREADS=1 ./pintegral 2 50000000
+echo ""
+OMP_NUM_THREADS=2 ./pintegral 2 50000000
+echo ""
+OMP_NUM_THREADS=4 ./pintegral 2 50000000
+echo ""
+OMP_NUM_THREADS=8 ./pintegral 2 50000000
+echo ""
+OMP_NUM_THREADS=16 ./pintegral 2 50000000
+echo ""
+echo ""
