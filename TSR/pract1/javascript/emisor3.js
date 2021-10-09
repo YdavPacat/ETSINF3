57696bb02dd0e14b59218c5f2d9d9f37dfabd30a
ydavpacat <ydavpacat21@gmail.com>
2021-10-09 16:55:20 +0200

    Acticadad 1 Pract1 TSR

--- TSR/pract1/javascript/emisor3.js
+++ TSR/pract1/javascript/emisor3.js
@@ -1,4 +1,5 @@
-
+const ev = require('events')
+const emitter = new ev.EventEmitter()
 
 const e1='e1', e2='e2'
 let inc=0, t
@@ -12,12 +13,18 @@ function rand() { // debe devolver valores aleat en rango [2000,5000) (ms)
 }
 function handler (e,n) { // e es el evento, n el valor asociado
  return (inc) => {
-   print(e + '-->' inc)
+   n += inc 
+   console.log(e + '-->' + n)
+   
  } // el oyente recibe un valor (inc)
 }
 emitter.on(e1, handler(e1,0))
 emitter.on(e2, handler(e2,''))
 function etapa() {
- ...
+    emitter.emit(e1, inc)
+    emitter.emit(e2, inc)
+    console.log(`Duración: ${t}`)
+    inc++;
+    setTimeout(etapa,t=rand())
 }
 setTimeout(etapa,t=rand())
