eed68ca261a1d2224ed06f9802ea6fa7a1df8de6
Jane <calizjane@gmail.com>
2021-10-17 21:01:05 +0200

    Update search.s

--- AIC/prac2/search.s
+++ AIC/prac2/search.s
@@ -1,14 +1,28 @@
+# cont = 0;
+# for (i = 0; i < n; i++) {
+#    if (a[i] == 0) {
+#        cont = cont + 1;
+#     }
+# }
+
+
         .data
-a:      .dword  9,8,0,1,0,5,3,1,2,0
+a:      .dword 9,8,0,1,0,5,3,1,2,0
 tam:    .dword 10       ; Tamaño del vector
 cont:   .dword 0        ; Número de componentes == 0
 
         .text
-start:	dadd r1,$gp,a   ; Puntero
-        ld r4,tam($gp)  ; Tamaño lista
-        dadd r10,r0,r0  ; Contador de ceros
+start:	dadd r1, $gp, a   ; Puntero
+        ld r4, tam($gp)   ; Tamaño lista
+        dadd r10, r0, r0  ; Contador de ceros
  
-loop:
-	...
+loop:   ld r12, 0(r1) 
+	bnez r12, if 
 
+        daddi r10, r10, 1
+
+if:     dsubi r4, r4, 1
+        daddi r1, r1, 8
+        bnez r4, loop
+        sd r10, cont($gp)
         trap #0
