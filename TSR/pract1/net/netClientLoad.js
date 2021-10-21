ecebf7548d3cffcb920312c2eb26f95aec34bc21
ydavpacat <ydavpacat21@gmail.com>
2021-10-21 11:28:37 +0200

    TSR IP

--- TSR/pract1/net/netClientLoad.js
+++ TSR/pract1/net/netClientLoad.js
@@ -6,14 +6,15 @@ if (process.argv.length < 4){
 }
 const IP_servidor = process.argv[2];  
 const IP_local = process.argv[3];
-const client = net.connect({port:8000, host: IP_servidor},
+const client = net.connect({port:8002, host: IP_servidor},
     function() { //connect listener
         console.log('client connected');
         client.write('world!\r\n');
+        
     });
 client.on('data',
  function(data) {
-        console.log(data.toString());
+        console.log(data + '');
         client.end(); //no more data written to the stream
         
     });
