2193847a407fb87916bafe50a402bad2f2023937
ydavpacat <ydavpacat21@gmail.com>
2021-11-25 13:26:12 +0100

    CPA pract3 ping ping

--- CPA/pract3/ping-pong.c
+++ CPA/pract3/ping-pong.c
@@ -11,7 +11,7 @@ char buf[NMAX];
 
 int main(int argc,char *argv[])
 {
-  int n, myid, numprocs;
+  int n, myid, numprocs, i;
   double f1, f2;
 
   MPI_Init(&argc,&argv);
@@ -32,7 +32,15 @@ int main(int argc,char *argv[])
      In each iteration, P0 sends a message of n bytes to P1, and P1 sends the same
      message back to P0. The data sent is taken from array buf and received into
      the same array. */
-
+  for(i = 0; i < NREPS; i++) {
+      if(myid == 0) {
+          MPI_Send(&buf, 1, MPI_BYTE, 1, 0, MPI_COMM_WORLD);
+          MPI_Recv(&buf, 1, MPI_BYTE, 1, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      } else {
+        MPI_Recv(&buf, 1, MPI_BYTE, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Send(&buf, 1, MPI_BYTE, 0, 1, MPI_COMM_WORLD);
+      }
+   }
 
   /* COMPLETE: Get current time, using MPI_Wtime() */
 
