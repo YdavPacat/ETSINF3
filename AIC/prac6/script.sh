208ca5839e5389fee8b604266664c6ac459860d6
ydavpacat <ydavpacat21@gmail.com>
2021-12-03 08:56:17 +0100

    AIC pract6

---
+++ AIC/prac6/script.sh
@@ -0,0 +1,11 @@
+echo "pnt"
+./mips-ooo -s -f suma-mat.s -p pnt
+echo "1"
+./mips-ooo -s -f suma-mat.s -p 1
+echo "2h"
+./mips-ooo -s -f suma-mat.s -p 2h
+echo "2s"
+./mips-ooo -s -f suma-mat.s -p 2s
+echo "p"
+./mips-ooo -s -f suma-mat.s -p p
+
